566a5539cd91d12d228adf4b900f37963816a5db
robrileyd <git@robandhel.co.uk>
2021-01-28 12:10:17 +0000

    actually added the k8hostfile.sh

---
+++ scripts/k8hostfile.sh
@@ -0,0 +1,7 @@
+#This is horrible and not idempotent!!!!
+
+sudo echo 172.16.94.10 c1-master1 >> /etc/hosts
+sudo echo 172.16.94.11 c1-node1 >> /etc/hosts
+sudo echo 172.16.94.12 c1-node2 >> /etc/hosts
+sudo echo 172.16.94.13 c1-node3 >> /etc/hosts
+sudo echo 172.16.94.14 c1-node4 >> /etc/hosts
